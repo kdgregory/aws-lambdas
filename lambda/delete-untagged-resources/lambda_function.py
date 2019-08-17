4014415001e8991de2cff3dcbad7c4763e9175b0
Keith Gregory <contact@kdgregory.com>
2019-08-17 09:36:56 -0400

    checkpoint

--- lambda/delete-untagged-resources/lambda_function.py
+++ lambda/delete-untagged-resources/lambda_function.py
@@ -8,26 +8,21 @@ logging.getLogger().setLevel(level=logging.INFO)
 
 def lambda_handler(event, context):
     for region in [ "us-east-1", "us-east-2", "us-west-1", "us-west-2"]:
-        processEC2(region)
-    
-def processEC2(region):
-    logging.info(f'processing EC2 region {region}')
-    client = boto3.client('ec2', region_name=region)
-    for instance in describeInstances(client):
-        logging.info(f'examining {json.dumps(instance)}')
-        
-def describeInstances(client):
-    result = []
-    paginator = client.get_paginator('describe_instances')
-    for page in paginator.paginate():
-        for reservation in page['Reservations']:
-            for instance in reservation['Instances']:
-                result.append(extractInstanceDetails(instance))
-    return result
-    
-def extractInstanceDetails(instance):
-    logging.debug(f"extracting details from instance {instance['InstanceId']}")
-    return {
-        'InstanceId': instance['InstanceId']
-    }
+        process_ec2(region)
 
+def process_ec2(region):
+    logging.info(f'processing EC2 region {region}')
+    ec2 = boto3.resource('ec2', region_name=region)
+    for instance in ec2.instances.all():
+        logging.debug(f'examining {instance.id}')
+        if has_no_name(instance):
+            logging.info(f'terminating {instance.id} because it has no name')
+            # instance.terminate()
+
+def has_no_name(instance):
+    if not instance.tags:
+        return True
+    for tag in instance.tags:
+        if tag['Key'] == 'Name':
+            return False
+    return True
